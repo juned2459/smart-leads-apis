f993b3050c2250abe2605bb7c8535df9b989a901
imjuned001 <promatics.juned@gmail.com>
2025-12-10 21:33:36 +0530

    nationalize api error

--- src/controllers/lead.controllers.js
+++ src/controllers/lead.controllers.js
@@ -1,19 +1,21 @@
 import Lead from "../models/lead.model.js";
 import { fetchNationality } from "../utils/fetchNationality.js";
+const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export const processBatch = async (req, res) => {
   try {
     const { names } = req.body;
-    const nameArr = names.split(",").map(name => name.trim());
+    const nameArr = names.split(",").map(n => n.trim());
 
-    const results = await Promise.all(
-      nameArr.map(async (name) => {
-        const userCountryData = await fetchNationality(name);
-        const status = userCountryData.probability > 0.6 ? "Verified" : "To Check";
-        const lead = await Lead.create({ name, country: userCountryData.country, probability: userCountryData.probability, status });
-        return lead;
-      })
-    );
+    const results = [];
+    for (let name of nameArr) {
+      await wait(300);
+      const userCountryData = await fetchNationality(name);
+      const status = userCountryData.probability > 0.6 ? "Verified" : "To Check";
+      const lead = await Lead.create({ name, country: userCountryData.country, probability: userCountryData.probability, status });
+      results.push(lead);
+    }
+    
     res.status(200).json({ success: true, data: results });
   } catch (err) {
     console.error(err);
--- src/utils/fetchNationality.js
+++ src/utils/fetchNationality.js
@@ -1,16 +1,30 @@
 import axios from "axios";
+const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export const fetchNationality = async (name) => {
-  const url = `https://api.nationalize.io?name=${name}`;
-  const res = await axios.get(url);
+  try {
+    const url = `https://api.nationalize.io?name=${name}`;
+    const res = await axios.get(url);
 
-  if (!res.data.country.length) {
+    if (!res.data.country.length) {
+      return { country: "Unknown", probability: 0 };
+    }
+
+    const best = res.data.country[0];
+    return {
+      country: best.country_id,
+      probability: best.probability
+    };
+
+  } catch (err) {
+    // If API limit reached → wait + retry
+    if (err.response?.status === 429) {
+      console.log("⚠️ Rate limit hit. Waiting 1.5 seconds...");
+      await wait(1500);
+      return fetchNationality(name); // retry
+    }
+
+    console.log("❌ API error:", err.message);
     return { country: "Unknown", probability: 0 };
   }
-
-  const best = res.data.country[0];
-  return {
-    country: best.country_id,
-    probability: best.probability
-  };
 };
